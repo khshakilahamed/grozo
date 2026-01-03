bbebbf5c13a50764b6659826b88b753ad413b1aa
khshakilahamed <khshakil.ahamed18@gmail.com>
2026-01-04 02:19:36 +0600

    register api

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,35 @@
+import connectDb from "@/lib/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+      try {
+            await connectDb();
+            const {name, email, password} = await request.json();
+
+            const existUser = await User.findOne({email});
+
+            if(existUser){
+                  return NextResponse.json({message: "User already exists"}, {status: 400});
+            }
+
+            if(password?.length < 6){
+                  return NextResponse.json(
+                        {message: "Password must be at least 6 characters"}, {status: 400});
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const newUser = await User.create({
+                  name,
+                  email,
+                  password: hashedPassword
+            });
+
+            return NextResponse.json(newUser, {status: 201});
+
+      } catch (error) {
+            return NextResponse.json({message: `Register error ${error}`}, {status: 500})
+      }
+}
