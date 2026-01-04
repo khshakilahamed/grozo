a1340f4a5dd64a84a392c9d69cb8dc7ac3258dad
khshakilahamed <khshakil.ahamed18@gmail.com>
2026-01-04 16:59:17 +0600

    auth.js integrate

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,70 @@
 import NextAuth from "next-auth"
- 
+import Credentials from "next-auth/providers/credentials"
+import connectDb from "./lib/db";
+import User from "./models/user.model";
+import bcrypt from "bcryptjs";
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
-})
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        await connectDb();
+
+        const email = credentials?.email;
+        const password = credentials?.password as string;
+
+        const user = await User.findOne({ email });
+        if (!user) {
+          throw new Error("User does not exist");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+          throw new Error("Invalid password");
+        }
+        return {
+          id: user._id.toString(),
+          email: user.email,
+          name: user.name,
+          role: user.role,
+        };
+      }
+    })
+  ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+        token.email = user.email;
+        token.role = user.role;
+      }
+
+      return token;
+    },
+
+    session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.role = token.role as string;
+      }
+
+      return session;
+    }
+  },
+  pages: {
+    signIn: '/login',
+    error: "/login"
+  },
+  session: { 
+    strategy: "jwt",
+    maxAge:  10 * 24 * 60 * 60 * 1000, // 10 days
+  },
+  secret: process.env.AUTH_SECRET,
+});
