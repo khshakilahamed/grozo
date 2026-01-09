91ba1bf9c23d0264015584f0ed2475b36ccbada7
khshakilahamed <khshakil.ahamed18@gmail.com>
2026-01-10 00:49:19 +0600

    cloudinary setup

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (file: Blob): Promise<string | null> => {
+      if (!file) {
+            return null;
+      }
+
+      try {
+            const arrayBuffer = await file.arrayBuffer();
+            const buffer = Buffer.from(arrayBuffer);
+
+            return new Promise((resolve, reject) => {
+                  const uploadStream = cloudinary.uploader.upload_stream(
+                        {
+                              resource_type: "auto",
+                              folder: "grozo/images"
+                        },
+                        (error, result) => {
+                              if (error) {
+                                    reject(error);
+                              } else {
+                                    resolve(result?.secure_url as string);
+                              }
+                        }
+                  )
+
+                  uploadStream.end(buffer);
+            })
+
+      } catch (error) {
+            console.log(error);
+            return null;
+      }
+}
+
+export default uploadOnCloudinary;
