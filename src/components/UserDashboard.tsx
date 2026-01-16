8249ce784939e31a10fd279068b2c2084132e1b5
khshakilahamed <khshakil.ahamed18@gmail.com>
2026-01-17 01:11:34 +0600

    grocery card

--- src/components/UserDashboard.tsx
+++ src/components/UserDashboard.tsx
@@ -1,12 +1,27 @@
+import connectDb from '@/lib/db';
 import CategorySlider from './CategorySlider';
 import HeroSection from './HeroSection';
+import Grocery, { IGrocery } from '@/models/grocery.model';
+import GroceryItemCard from './GroceryItemCard';
 
-const UserDashboard = () => {
+const UserDashboard = async () => {
+      await connectDb();
+      const groceries = await Grocery.find({}).lean();
+      const plainGroceries = JSON.parse(JSON.stringify(groceries));
+
       return (
-            <div>
+            <>
                   <HeroSection />
-                  <CategorySlider/>
-            </div>
+                  <CategorySlider />
+                  <div className='w-[90% md:w-[80%] mx-auto mt-10]'>
+                        <h2 className='text-2xl md:text-3xl font-bold text-green-700 mb-6 text-center'>Grocery Items</h2>
+                        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6'>
+                              {plainGroceries?.map((item: IGrocery) => (
+                                    <GroceryItemCard key={String(item?._id)} item={item} />
+                              ))}
+                        </div>
+                  </div>
+            </>
       );
 };
 
