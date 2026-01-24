3711e85dc03feac5bc302166057e8b64a40e3f36
khshakilahamed <khshakil.ahamed18@gmail.com>
2026-01-24 12:08:23 +0600

    delivery assignment model

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -27,6 +27,8 @@ export interface IOrder {
             latitude: number;
             longitude: number;
       };
+      assignment?: mongoose.Types.ObjectId;
+      assignedDeliveryBoy?: mongoose.Types.ObjectId;
       status: "pending" | "out of delivery" | "delivered";
       createdAt?: Date;
       updatedAt?: Date;
@@ -78,6 +80,15 @@ const orderSchema = new mongoose.Schema<IOrder>({
             fullAddress: String,
             latitude: Number,
             longitude: Number,
+      },
+      assignment: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "DeliveryAssignment",
+            default: null,
+      },
+      assignedDeliveryBoy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
       },
       status: {
             type: String,
